c5e8435337bb3b2da5ed19e2df52741e59620ba8
Raivo Koot <raivokoot@gmail.com>
2018-10-27 19:55:33 +0100

    IDF algorithm implemented
    IDF Algorithm now correctly adds an extra column to ingredients_compounds matrix labelled uniqueness that contains the tf idf of that compound for that ingredient

--- python model/MainTesting.py
+++ python model/MainTesting.py
@@ -2,6 +2,7 @@
 from matrixoperations import MatrixOperations
 from foodData import FoodData
 from dataPreparation import DataPreparation
+from compoundEvaluator import CompoundEvaluator
 
 matrixOps = MatrixOperations()
 
@@ -9,10 +10,16 @@ matrixOps = MatrixOperations()
 #titles = ingredientData['title'].values
 
 datasetContainer = FoodData()
-
+datasetContainer.importIngredientDataSet('ingredient_info.tsv')
+datasetContainer.importCompoundDataSet('compound_info.tsv')
 datasetContainer.importIngredientsCompoundstDataSet('ingredients_compounds.tsv')
 
-
 datasetPreparer = DataPreparation()
-
 datasetPreparer.addMatchUniquenessColumn(datasetContainer)
+
+compoundEvaluator = CompoundEvaluator()
+compoundEvaluator.computeIDF(datasetContainer)
+compoundEvaluator.assignUniquenesses(datasetContainer)
+
+# labelled eaech pair of ingredient and compound with uniqueness value
+print(datasetContainer.ingredientsCompoundsDataSet)
