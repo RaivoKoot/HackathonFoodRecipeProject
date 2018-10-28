82c5bc02448caa5bd603cf92937165f440d6a78b
Rollyne <rollyne@abv.bg>
2018-10-28 03:37:39 +0300

    Added front-end; Added view functions and templates

--- recipeProject/main/views.py
+++ recipeProject/main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Recipe
+from django.template import loader
+from django.http import Http404
 
-# Create your views here.
+def recipes_index(request):
+
+    latest_recipes = Recipe.objects.order_by('-date_published')[:6]
+    context = {'latest_recipes':latest_recipes,}
+
+    return render(request, 'index.html', context)
+
+
+def recipes_detail(request, id):
+
+    recipe = None
+
+    try:
+        recipe = Recipe.objects.get(id = id)
+    except:
+        return Http404('Recipe not found!')
+
+    context = {'recipe': recipe}
+
+    return render(request, 'pages/recipe.html', context)
