65fe1621e933ec36e44b4b51d5bf267cb2c70b4f
Rollyne <rollyne@abv.bg>
2018-10-28 09:47:05 +0200

    Finished the similarity model

---
+++ data_processing/similarity_model.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from collections import OrderedDict
+
+
+def ingredient_compound_binary():
+    ingredient_details = pd.read_csv("../python model/data/ingredient_info.tsv", sep='\t')
+    ingredients_compouds = pd.read_csv("../python model/data/ingredients_compounds.tsv", sep='\t')
+    compound_details = pd.read_csv("../python model/data/compound_info.tsv", sep='\t')
+
+    ingredients_compounds_joined = ingredient_details\
+        .set_index("# id").join(ingredients_compouds.set_index("# ingredient id"))\
+        .set_index("compound id").join(compound_details.set_index("# id"))\
+        .drop(columns=["category", "CAS number"])
+
+    compound_names = ingredients_compounds_joined[['Compound name']].drop_duplicates()
+    ingredient_names = ingredients_compounds_joined[['ingredient name']].drop_duplicates()
+
+    ingredient_names.to_pickle("data/ingredient_names.pkl")
+
+    compounds = compound_names.values[:,0]
+    ingredients = ingredient_names.values[:,0]
+
+    ingredient_compounds_binary = []
+
+    # for ingredient in ingredients:
+    #     bin = []
+    #     for compound in compounds:
+    #         if
+    for ingr in ingredients:
+        ingredient_compoud = ingredients_compounds_joined.ix[ingredients_compounds_joined['ingredient name'] == ingr][['Compound name']].values[:,0]
+        compounds_binary = ([1 if compound in ingredient_compoud else 0 for compound in compounds])
+        ingredient_compounds_binary.append(compounds_binary)
+
+    pd.DataFrame(ingredient_compounds_binary).to_pickle("data/ingredient_compounds_binary.pkl")
+
+
+def get_recipe_ingredients_cos_similarity():
+
+    ingredient_compounds_binary = pd.read_pickle("data/ingredient_compounds_binary.pkl")
+
+    transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(ingredient_compounds_binary).toarray()
+    cos = cosine_similarity(tfidf)
+
+    return cos
+
+
+def save_cos_similarities_to_pkl():
+    """
+    Gets and saves in pickle files the cosine similarities of the ingredients for both compounds and recipes and includes the
+    ingredient labels for the columns and rows. The format of the sets is pandas DataFrame.
+    """
+
+    cos_compounds = pd.DataFrame(get_recipe_ingredients_cos_similarity())
+    ingredient_names = pd.read_pickle("data/ingredient_names.pkl")
+
+    cos_compounds.columns = ingredient_names.values[:, 0]
+    cos_compounds.index = ingredient_names.values[:, 0]
+
+    cos_compounds.to_pickle("data/cos_compounds.pkl")
+
+
+def get_top_replacements(ingredient_name: str, top_n: int = 5):
+
+    cos_compounds = pd.read_pickle("data/cos_compounds.pkl")[ingredient_name]
+
+    d_descending = OrderedDict(sorted(cos_compounds.items(), key=lambda kv: kv[1], reverse=True))
+
+    result = {}
+
+    i = 0
+    for k, v in d_descending.items():
+        i += 1
+        if (i > top_n + 1):
+            break
+        if (k != ingredient_name):
+            result[k] = v
+
+    return result
+
+
+if __name__ == "__main__":
+    # ingredient_compound_binary()
+    print(get_top_replacements(input("Ingredient name: ")))
