3c5107fbca2569315f3cc6fefd2394a1b394c89a
Raivo Koot <raivokoot@gmail.com>
2018-10-28 01:29:22 +0000

    Scipts to get matrix of cosine difference (ingredientMatrix) complete
    New Class IngredientMatcher now uses method computeIngredientMatrix to create a matrix that maps every ingredient to every ingredient in a cell of their cosine difference.
    
    This matrix will be used to find Ingredients that are similar to each other.

--- python model/MainTesting.py
+++ python model/MainTesting.py
@@ -5,6 +5,12 @@ from compoundEvaluator import CompoundEvaluator
 from ingredientMatcher import IngredientMatcher
 import numpy as np
 
+############################
+# the code is commented out in multiline comments because
+# it only needs to be executed once and saves every
+# new data in files to be used externally
+
+
 # load datasets into foodData object
 datasetContainer = FoodData()
 datasetContainer.importIngredientDataSet('ingredient_info.tsv')
@@ -27,15 +33,30 @@ compoundEvaluator.assignUniquenesses(datasetContainer)
 
 '''
 datasetContainer.exportMatchesToFile()
-'''
-
 datasetContainer.importMatches()
+'''
 
 #print(datasetContainer.ingredientsCompoundsDataSet)
 
 # fills the ingredient-compound matching matrix with their calculated idfs
+
 ingredientMatcher = IngredientMatcher()
+'''
 ingredientMatcher.fitIDFMatrix(datasetContainer)
+'''
+
 
 # saves filled matchingMatrix in a csv file
-datasetContainer.exportMatchingMatrix()
+'''
+datasetContainer.exportCompoundMatrix()
+'''
+
+'''
+datasetContainer.importCompoundMatrix()
+'''
+
+# get a comparison matrix of all ingredients
+'''
+ingredientMatcher.computeIngredientMatrix(datasetContainer)
+datasetContainer.exportIngredientMatrix()
+'''
